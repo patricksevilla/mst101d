ff2523ee8374388936a637d66d6ca17dca2ff8cb
patricksevilla <57100910+patricksevilla@users.noreply.github.com>
2022-03-29 07:34:10 +0800

    lt2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+
+
+import React from 'react'
+import Navbar from './components/Navbar.js'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <>
+    <Router>
+      <Navbar/>
+      <Switch>
+        <Route path='/'exact>
+
+        </Route>
+      </Switch>
+    </Router>
+    </>
+    
+
+
+  //   {/* <div className="App">
+  //     <a href="https://bulsu.edu.ph/">Bulacan State Universty</a>
+
+  //     <div className='iicon'>
+  //     <i className='fab fa-github-square' />
+  //     <i className='fas fa-user' />
+  //     <i className='fas fa-times' />
+  //     <i className='fas fa-bars' />
+  //     </div> */}
+
+
+  //     {/* <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header> */}
+  //   // </div>
   );
 }
 
