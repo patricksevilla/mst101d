f7dd8fa64edaaf831337533f7f9e144656eb562a
patricksevilla <57100910+patricksevilla@users.noreply.github.com>
2022-04-06 12:44:50 +0800

    fixing mobile view centering title and paragraph

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,8 +7,12 @@ function HeroSection() {
   return (
     <div className='hero-container'>
       <video src='/videos/ce.mp4' autoPlay loop muted />
-      <h1>BUILDING TOMORROWS COMMUNITY</h1>
-      <p>A Comprehensive Guide to Civil Engineering Program</p>
+      <h1 text-align='center' >BUILDING TOMORROWS COMMUNITY</h1>
+      <p text-align='center' text-align-last='center' word-break= 'break-all' word-wrap= 'break-word' text-justify = 'inter-word'> 
+        A Comprehensive Guide to Civil Engineering Program
+      </p>
+
+ 
       <div className='hero-btns'>
         <Button
           className='btns'
